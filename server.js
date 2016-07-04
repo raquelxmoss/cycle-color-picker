ae8692ac842bf1c7c9c00ef971fa2265a4c8b2c2
Raquel Moss <raquelxmoss@gmail.com>
2016-07-04 21:08:12 +1200

    Add sass and attempt to get livereloading working

--- server.js
+++ server.js
@@ -1,12 +1,32 @@
 import budo from 'budo';
 import babelify from 'babelify';
+import sass from 'node-sass-middleware';
+import path from 'path';
 
-budo('./index.js', {
+const middleware = sass({
+  src: __dirname,
+  dest: __dirname,
+  debug: true,
+  outputStyle: 'compressed'
+});
+
+var app = budo('./index.js', {
   serve: 'bundle.js',
-  live: '*.{css,html}',
   port: 8000,
+  live: '*.{html, scss}',
   stream: process.stdout,
   browserify: {
     transform: babelify
+  },
+  middleware: middleware
+})
+.live()
+.watch(['*.{html, scss}'])
+.on('watch', function (type, file) {
+  if (path.extname(file) === '.scss') {
+    app.reload('styles.css');
+  } else {
+    app.reload(file);
   }
-});
+})
+.on('pending', () => app.reload());
