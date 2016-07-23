595c93566f40883d9ded775c90f1abded65ebfcc
Raquel Moss <raquelxmoss@gmail.com>
2016-07-23 12:14:13 +1200

    Test that ColorPicker returns an rgba string when required

--- test/color-picker-test.js
+++ test/color-picker-test.js
@@ -7,19 +7,29 @@ import { mockDOMSource } from '@cycle/dom';
 import rxAdapter from '@cycle/rx-adapter';
 
 describe('ColorPicker', () => {
+  const Mouse = {
+    up: () => Observable.empty()
+  };
+  const DOM = mockDOMSource(rxAdapter, {});
+
   it('takes a props$ of color and returns a color$', (done) => {
-    const props$ = Observable.of({color: '#fff'});
-
-    const Mouse = {
-      up: () => Observable.empty()
-    };
-
-    const DOM = mockDOMSource(rxAdapter, {});
-
+    const props$ = Observable.of({color: 'red'});
     const colorPicker = ColorPicker({Mouse, props$, DOM});
 
-    colorPicker.color$.take(1).subscribe((color) => {
-      assert.equal(color, 'rgb(255, 255, 255)');
+    colorPicker.color$.skip(1).take(1).subscribe((color) => {
+      assert.equal(color, 'rgb(255, 0, 0)');
+      done();
+    });
+  });
+
+  it('returns an rgba when alpha is required', (done) => {
+    const expectedColor = 'rgba(255, 255, 255, 0.5)';
+    const props$ = Observable.of({color: expectedColor});
+
+    const colorPicker = ColorPicker({DOM, Mouse, props$});
+
+    colorPicker.color$.skip(1).take(1).subscribe((color) => {
+      assert.equal(color, expectedColor);
       done();
     });
   });
