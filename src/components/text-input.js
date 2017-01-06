2d0d799a796d9b1d8caaac0946010f30df6f61b8
Raquel Moss <raquelxmoss@gmail.com>
2017-01-07 11:47:55 +1300

    able to render hex/rgba/hsla inputs

--- src/components/text-input.js
+++ src/components/text-input.js
@@ -5,15 +5,62 @@
 // also you can cycle through the inputs, but that doesn't affect the outside world, that's internal state
 //
 // Props: color, format
-//
 
 import xs from 'xstream';
 import debounce from 'xstream/extra/debounce';
 import tinycolor from 'tinycolor2';
-import {input} from '@cycle/dom';
+import { input, div, span } from '@cycle/dom';
+
+import { isInt } from '../helpers';
+
+const colorInputViews = {
+  hex: (color) => hexView(color.toHexString()),
+  rgba: (color) => colorInputView(color.toRgb()),
+  hsla: (color) => colorInputView(color.toHsl())
+};
+
+function makeInputElement (inputType, color, channel) {
+  const value = isInt(color[channel]) ? color[channel] : color[channel].toFixed(2);
+
+  return (
+    input(
+      `.${inputType}-input .text-input`,
+      {
+        props: {
+          value,
+          'data-channel': channel
+        }
+      }
+    )
+  );
+}
+
+function colorInputView (color) {
+  const inputType = Object.keys(color);
+
+  return (
+      div('.color-input-container',
+        inputType.map((channel) => {
+          return div('.channel-container', [
+            makeInputElement(inputType, color, channel),
+            span(channel)
+          ]);
+        }
+      )
+    )
+  );
+}
+
+function hexView (color) {
+  return input('.hex-input', {props: {type: 'text', value: color}});
+}
 
 function view (color) {
-  return input('.hex-input', {props: {type: 'text', value: color}});
+  const format = 'hsla'; // either hex, rgba, hsla
+
+  return div('.color-display', [
+    colorInputViews[format](tinycolor(color))
+  ]);
 }
 
 function colorFromInput (colorString) {
@@ -26,72 +73,22 @@ function colorFromInput (colorString) {
 
 export default function TextInput ({DOM, color$}) {
   const input$ = DOM
-    .select('.hex-input')
+    .select('.color-input-container input')
     .events('input')
     .compose(debounce(300))
     .map(ev => ev.target.value);
 
-  const hexFromProps$ = color$.map(color => tinycolor.fromRatio(color).toHexString());
+  const colorFromProps$ = color$.map(color => tinycolor.fromRatio(color).toHexString());
 
-  const hexFromInput$ = input$.map(colorFromInput);
+  const colorFromInput$ = input$.map(colorFromInput);
 
-  const hex$ = xs.merge(
-    hexFromProps$,
-    hexFromInput$
-  ).startWith('#FFFFFF')
+  const colorChange$ = xs.merge(
+    colorFromProps$,
+    colorFromInput$
+  ).startWith('#FFFFFF');
 
   return {
-    DOM: hex$.map(view),
-    change$: hexFromInput$.map(color => tinycolor(color).toHsv())
+    DOM: colorChange$.map(view),
+    change$: colorFromInput$.map(color => tinycolor(color).toHsv())
   };
 }
-
-// function makeInputElement (inputType, color, channel) {
-//   const value = isInt(color[channel]) ? color[channel] : color[channel].toFixed(2);
-
-//   return (
-//     input(
-//       `.${inputType}-input .text-input`,
-//       {
-//         props: {
-//           value,
-//           'data-channel': channel
-//         }
-//       }
-//     )
-//   );
-// }
-
-// function colorInputView (color) {
-//   const inputType = Object.keys(color).join('');
-
-//   return (
-//       div('.color-input-container',
-//         color.map((value, channel) => {
-//           return div('.channel-container', [
-//             makeInputElement(inputType, color, channel),
-//             span(channel)
-//           ]);
-//         }
-//       )
-//     )
-//   );
-// }
-
-// function hexView (color) {
-// }
-
-// const colorInputViews = {
-//   hex: (color) => hexView(color.toHexString()),
-//   rgba: (color) => colorInputView(color.toRgb()),
-//   hsla: (color) => colorInputView(color.toHsl())
-// };
-
-// function renderColorInputs (state) {
-//   const format = state.colorInputFormat.value;
-//   const color = tinycolor.fromRatio(state.color);
-
-//   return div('.color-display', [
-//     colorInputViews[format](color)
-//   ]);
-// }
